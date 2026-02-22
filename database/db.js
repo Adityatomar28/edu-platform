8f6fa1b83cf3ee0732586f3dd2415d6137893e36
Adityatomar28 <adityasinghtomar52gmail.com>
2026-02-23 03:15:52 +0530

    Backend progress with models and db

---
+++ database/db.js
@@ -0,0 +1,109 @@
+import mongoose, { mongo } from "mongoose";
+
+//How many it should try
+const MAX_RETRIES = 3
+const RETRY_INTERVAL = 5000;
+
+
+class DatabaseConnection{
+    
+    //It prepares for basic stuff
+    constructor(){
+        this.retryCount = 0
+        this.isConnected = false
+         
+        //configure mongoose setting
+        mongoose.set("strictQuery",true);
+
+        mongoose.connection.on('connected',()=>{
+            console.log("MongoDB CONNECTED successfully")
+            this.isConnected = true;
+        });
+         mongoose.connection.on('error',()=>{
+            console.log("MongoDB Connection error")
+            this.isConnected = false;
+        });
+         mongoose.connection.on('disconnected',()=>{
+            console.log("MongoDB Disconnected")
+            this.isConnected = false;
+            this.handleDisConnection()
+        });
+        //this is the constructor and you are calling method outside the constructor it has no idea which method is called that's why context should be passed 
+        process.on('SIGTERM',this.handleAppTermination.bind(this))
+
+    }
+    //CONNECTION
+    async connect() {
+        try{
+        if(!process.env.MONGO_URI){
+            throw new Error("Mongo db URI is not defined in env variable");
+        }
+        const connectionOptions ={
+            UserNewUrlParser:true,
+            useUnifiedTopology:true,
+            maxPoolSize:10,
+            serverSelectionTimeoutMS:5000,
+            socketTimeoutMS:45000,
+            family:4 //use IPv4
+        
+        };
+        if(process.env.NODE_ENV === "development"){
+            mongoose.set('debug',true)
+        }
+        await mongoose.connect(process.env.MONGO_URI,connectionOptions);
+        this.retryCount = 0 //reset retry count on success
+        
+
+    }catch (error){
+        console.error(error.message)
+        await this.handleConnectionError()
+    }
+}
+    // connection ERROR 
+    async handleConnectionError(){
+        if(this.retryCount <MAX_RETRIES){
+            this.retryCount++;
+            console.log(`Retrying connection... Attemp ${this.retryCount} of ${MAX_RETRIES}`)
+            await new Promise(resolve => setTimeout(() => {
+                resolve
+            },RETRY_INTERVAL))
+            return this.connect()
+
+
+        }else{
+            console.error(`Failed to connect to MONGODB after ${MAX_RETRIES}`)
+        }
+    }
+
+    async handleDisConnection(){
+        if(!this.isConnected){
+         console.log("Attempting to reconnect to mongodb...")
+         this.connect()
+        }
+    }
+    async handleAppTermination(){
+        try {
+            await mongoose.connection.close()
+            console.log("MongoDb connection closed through app terminator")
+            process.exit(0);
+        } catch (error) {
+            console.error('Error during database disconnection',error)
+            process.exit(1)
+        }
+    }
+    getConnectionStatus(){
+        return{
+            isConnected:this.isConnected,
+            readyState:mongoose.connection.host,
+            host:mongoose.connection.host,
+            name:mongoose.connection.name,
+        }
+    }
+}
+
+//create a singleton instance
+
+const dbConnection = new DatabaseConnection()
+
+export default dbConnection.connect.bind(dbConnection)
+export const getConnectionStatus = dbConnection.getConnectionStatus.bind(dbConnection)
---
+++ models/user.model.js
@@ -0,0 +1,112 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import crypto, { createDecipheriv } from "crypto";
+
+
+
+const UserSchema = new mongoose.Schema({
+    //here u see objects 
+    name:{
+    type:String,
+    required:[true,'Name is required'],
+    trim:true,
+    MaxLength:[50,'Name cannot exceed 50 character']
+    },
+    email:{
+    type:String,
+    required:[true,'Email is required'],
+    trim:true,
+    unique:true,
+    lower:true,
+    //Regexr
+    match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Please provide a valid email"],
+    },
+    Password:{
+    type:String,
+    required:[true,'password is required'],
+    MinLength:[8,'Password must be at least 8 character'],
+    select:false
+    },
+    role:{
+        type:String,
+        //it is like option that we are booking seat in bus we have options of selecting side,upper,middle
+        enum:{
+            values:['student','instructor','admin'],
+            message:'Please select a valid role'
+        },
+        default:'student'
+
+    },
+    avatar:{
+        type:String,
+        default:'default-avatar.png'
+    },
+    bio:{
+        type:String,
+        maxLength:[200,'Bio cannot exceed 200 character']
+    },
+    enrolledCourses:[{
+        //what course you are enrolled in refer one of the course
+        course:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Course'
+        },
+        enrolledCourses:{
+            type:Date,
+            default:Date.now
+        }
+    }],
+    createdCourses:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Courses'
+    }],
+    resetPasswordToken:String,
+    resetPasswordExpiry:Date,
+    lastActive:{
+        type:Date,
+        default:Date.now
+    }
+},{
+    timestamps:true,
+    toJSON:{virtuals:true},
+    toObject:{virtuals:true}
+
+});
+//hashing the Password
+//use async because when you hash your password it goes to multiple round of cryptography takes little time 
+//use function not arrow function beacuse they need to know the context
+UserSchema.pre('save',async function(next){
+    if(!this.isModified('password')){
+        return next();
+    }
+    //next is a hook which is a middleware
+    this.password = await bcrypt.hash(this.Password,12)
+    next();
+});
+//compare password when user log in it check from hashed password 
+UserSchema.methods.comparePassword = async function(enterPassword){
+    return await bcrypt.compare(enterPassword,this.password)
+}
+UserSchema.methods.getResetPasswordToken = function(){
+    const resetToken = crypto.randomBytes(20).toString('hex')
+    this.resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex')
+        this.resetPasswordExpiry = Date.now() + 10*60*1000 //10min
+        return resetToken
+
+        // one token is sent to te database and one token is sent to user 
+}
+//Virtuals ->calacuaktive filed suppose i want to find how mnay course you are enrolled in ?
+// UserSchema.methods.updateLastActive = function(){
+    this.lastActive = Date.now()
+    return this.lastActive({validateBeforeSave:false});
+
+UserSchema.virtual('totalEnrolledCourses').get(function(){
+    this.enrolledCourses.length
+})
+
+
+//in this User is a standard way to call user and in database all got converted in lowercase
+export const User = mongoose.model('User',UserSchema)
